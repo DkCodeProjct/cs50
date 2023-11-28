0b2a0d68797475c6b7051edcf6ffeeac9a784834
DKcodeX <148317495+DkCodeProjct@users.noreply.github.com>
2023-11-28 15:27:56 +0530

    prof. David’s first toys as a child</. little Professor calculator
    "This is a CS50 problem I've been grinding on, and I finally got a chance to wrap my head around it. I kind of feel enlightened after that, though.

--- new.py
+++ new.py
@@ -1,33 +1,71 @@
 import random
 
-def get_user_input(prompt):
-    while True:
-        try:
-            user = int(input(prompt))
-            if user > 0:
-                return user
-            else:
-                print("negative_integer")
-        except ValueError:
-            print("enter a valid input!")
-
 def main():
-    print("welcme_to_the_game>>> ")
-    num = get_user_input("chose a number ")
-    gen_num = random.randint(1, num)
+    level = getLevel()
+    score = simulateGame(level)
+    print(f"score:_ 5/{score}")
 
-    attempts = 0
+def getLevel():
     while True:
-        guess = get_user_input("guess_THE_number (1 - {}): ".format(num))
-        attempts += 1
+      try:
+         level = int(input("choose level[1/2/3] "))
+      except ValueError:
+         pass
+      
+      else:
+         if level in [1, 2, 3]:
+            return level
+         else:
+            print("level was not in [1/2/3]")
 
-        if guess > gen_num:
-            print("greater_than THE NUMBER")
-        elif guess < gen_num:
-            print("less_than THE NUMBER")
-        else:
-            print("CORRECT !")
-            break
+def genNum(level):
+   
+   if level == 1:
+      x = random.randint(0, 9)
+      y = random.randint(0, 9)
+  
+   elif level == 2:
+      x = random.randint(9, 99)
+      y = random.randint(9, 99)
+   
+   else:
+      x = random.randint(99, 999)
+      y = random.randint(99, 999)
+   
+   return x, y
+
+def simulateRound(x, y):
+   tries = 3
+   while tries > 0:
+      try:
+         getUsreAnswer = int(input(f"{x}+{y}= "))
+      except ValueError:
+         pass
+      else:
+         
+         if getUsreAnswer == (x+y):
+            return True
+         else:
+            print("EEE")
+            tries -= 1
+   
+   print(f"{x}+{y}= {x+y} ")
+
+def simulateGame(level):
+   score = 0
+   for i in range(5):
+      
+      x,y = genNum(level)
+      if simulateRound(x,y):
+         
+         print("correct")
+         score += 1
+      
+      else:
+         print("EEE")
+   return score
 
 if __name__ == "__main__":
-    main()
+   main()
+      
+
