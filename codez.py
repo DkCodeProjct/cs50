b417bb4b844a98ef889e7ca22abb302a6cdcd3ab
DKcodeX <148317495+DkCodeProjct@users.noreply.github.com>
2024-02-02 14:39:15 +0530

    CS50 Adieu, adieu prblmSet4
    this is my first time messing with problem when i first saw problem i didnt liked it and just passed,,, because i didn't up problem's for submit50,,, 
              but now I'm uploading and  so try to tackle the problem and it did go well,,,,,,,,,,,,,,,>>>>>
                     problem was simple im just naïve to look at it,,,,, Here"S the PROBLEM put comma before the last word LIKE THIS ---> > > X , X , X and X
                               get name in loop and break with EOFError and print NAMES.............
    since my EOFError[^d] didnt work on vsCode i put ... to bresk

---
+++ codez.py
@@ -0,0 +1,35 @@
+import inflect
+
+#put  .. instead EOFEror work as expected
+
+p = inflect.engine()
+c = []
+while True:
+    
+    getname = input('names ')
+    c.append(getname)
+    if getname == '..':
+        print()
+        break 
+outPut = p.join(c)
+print(outPut)
+
+
+"""
+#work as exceptet BUT if EORError works correctly it didnt getting ^d
+import inflect
+
+p = inflect.engine()
+c = []
+
+while True:
+    try:
+        names = input('name: ')
+        c.append(names)
+    except EOFError():
+        print()
+        break
+outPut= p.join(c)
+print('Adiue adiue to', outPut)
+
+""" 
